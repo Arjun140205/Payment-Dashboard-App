bd1cfc6cfa3bf7d3cc01f03d31a0227a06ec45e8
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-07-11 00:15:16 +0530

    modified: server/src/payments/payments.controller.ts

--- server/src/payments/payments.controller.ts
+++ server/src/payments/payments.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get('stats')
   getStats() {
     return this.paymentsService.getStats();
