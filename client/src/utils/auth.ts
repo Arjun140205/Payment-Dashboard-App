3117b80ec400cd7104aa3cf08af86f62a87d662f
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-07-11 00:15:15 +0530

    modified: client/src/utils/auth.ts

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,15 +1,28 @@
 import * as SecureStore from 'expo-secure-store';
+import { Platform } from 'react-native';
 
 const TOKEN_KEY = 'jwt_token';
 
 export const saveToken = async (token: string) => {
-  await SecureStore.setItemAsync(TOKEN_KEY, token);
+  if (Platform.OS === 'web') {
+    localStorage.setItem(TOKEN_KEY, token);
+  } else {
+    await SecureStore.setItemAsync(TOKEN_KEY, token);
+  }
 };
 
 export const getToken = async () => {
-  return await SecureStore.getItemAsync(TOKEN_KEY);
+  if (Platform.OS === 'web') {
+    return localStorage.getItem(TOKEN_KEY);
+  } else {
+    return await SecureStore.getItemAsync(TOKEN_KEY);
+  }
 };
 
 export const clearToken = async () => {
-  await SecureStore.deleteItemAsync(TOKEN_KEY);
+  if (Platform.OS === 'web') {
+    localStorage.removeItem(TOKEN_KEY);
+  } else {
+    await SecureStore.deleteItemAsync(TOKEN_KEY);
+  }
 };
