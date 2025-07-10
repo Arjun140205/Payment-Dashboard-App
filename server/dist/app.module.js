4b93e462930e8d925ec9bbfa468b451438b9f562
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-07-10 23:49:40 +0530

    build: updated compiled app.module.js

--- server/dist/app.module.js
+++ server/dist/app.module.js
@@ -11,12 +11,13 @@ const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const auth_module_1 = require("./auth/auth.module");
+const payments_module_1 = require("./payments/payments.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [auth_module_1.AuthModule],
+        imports: [auth_module_1.AuthModule, payments_module_1.PaymentsModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
